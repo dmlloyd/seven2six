fe61a7a500a4aec9ade9c3e6c52c1634a12e58be
David M. Lloyd <david.lloyd@redhat.com>
2013-08-30 13:17:45 -0500

    Initial commit

---
+++ src/main/java/org/jboss/seven2six/Translator.java
@@ -0,0 +1,160 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2013 Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.jboss.seven2six;
+
+import java.io.Closeable;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.RandomAccessFile;
+import java.lang.instrument.ClassFileTransformer;
+import java.lang.instrument.IllegalClassFormatException;
+import java.security.ProtectionDomain;
+
+import org.objectweb.asm.ClassReader;
+import org.objectweb.asm.ClassVisitor;
+import org.objectweb.asm.ClassWriter;
+import org.objectweb.asm.Handle;
+import org.objectweb.asm.MethodVisitor;
+import org.objectweb.asm.Opcodes;
+
+/**
+ * Translate JDK 7 classes to JDK 6.
+ *
+ * @author <a href="mailto:david.lloyd@redhat.com">David M. Lloyd</a>
+ */
+public final class Translator implements ClassFileTransformer {
+
+    public Translator() {
+    }
+
+    /**
+     * Translate all {@code .class} files in the given list of files and directories.
+     *
+     * @param args the file and directory names
+     */
+    public static void main(String[] args) {
+        new Translator().transformRecursive(args);
+    }
+
+    /**
+     * Translate all {@code .class} files in the given list of files and directories.
+     *
+     * @param names the file and directory names
+     */
+    public void transformRecursive(String... names) {
+        final File[] files = new File[names.length];
+        for (int i = 0; i < names.length; i++) {
+            files[i] = new File(names[i]);
+        }
+        transformRecursive(files);
+    }
+
+    /**
+     * Translate all {@code .class} files in the given list of files and directories.
+     *
+     * @param files the files and directories
+     */
+    public void transformRecursive(File... files) {
+        for (File file : files) {
+            if (file.isDirectory()) {
+                transformRecursive(file.listFiles());
+            } else if (file.getName().endsWith(".class")) {
+                try {
+                    transform(new RandomAccessFile(file, "rw"));
+                } catch (IllegalClassFormatException | IOException e) {
+                    System.err.println("Failed to transform " + file + ": " + e);
+                }
+            }
+            // else ignore
+        }
+    }
+
+    public byte[] transform(final ClassLoader loader, final String className, final Class<?> classBeingRedefined, final ProtectionDomain protectionDomain, final byte[] classfileBuffer) throws IllegalClassFormatException {
+        ClassWriter classWriter = new ClassWriter(0);
+        new ClassReader(classfileBuffer).accept(new TranslatingClassVisitor(classWriter), 0);
+        return classWriter.toByteArray();
+    }
+
+    public byte[] transform(final InputStream input) throws IllegalClassFormatException, IOException {
+        ClassWriter classWriter = new ClassWriter(0);
+        new ClassReader(input).accept(new TranslatingClassVisitor(classWriter), 0);
+        return classWriter.toByteArray();
+    }
+
+    public void transform(final InputStream input, final OutputStream output) throws IllegalClassFormatException, IOException {
+        output.write(transform(input));
+    }
+
+    public void transform(final RandomAccessFile file) throws IllegalClassFormatException, IOException {
+        try {
+            file.seek(0);
+            final InputStream is = new FileInputStream(file.getFD());
+            try {
+                final byte[] bytes = transform(is);
+                file.seek(0);
+                file.write(bytes);
+                file.setLength(bytes.length);
+                is.close();
+            } finally {
+                safeClose(is);
+            }
+            file.close();
+        } finally {
+            safeClose(file);
+        }
+    }
+
+    private static void safeClose(final Closeable closeable) {
+        if (closeable != null) try {
+            closeable.close();
+        } catch (Throwable ignored) {}
+    }
+
+    private static class TranslatingClassVisitor extends ClassVisitor {
+
+        public TranslatingClassVisitor(final ClassWriter classWriter) {
+            super(Opcodes.ASM4, classWriter);
+        }
+
+        public void visit(final int version, final int access, final String name, final String signature, final String superName, final String[] interfaces) {
+            super.visit(version == Opcodes.V1_7 ? Opcodes.V1_6 : version, access, name, signature, superName, interfaces);
+        }
+
+        public MethodVisitor visitMethod(final int access, final String name, final String desc, final String signature, final String[] exceptions) {
+            final MethodVisitor defaultVisitor = super.visitMethod(access, name, desc, signature, exceptions);
+            return new MethodVisitor(Opcodes.ASM4, defaultVisitor) {
+                public void visitInvokeDynamicInsn(final String name, final String desc, final Handle bsm, final Object... bsmArgs) {
+                    // todo: fail
+                    super.visitInvokeDynamicInsn(name, desc, bsm, bsmArgs);
+                }
+
+                public void visitMethodInsn(final int opcode, final String owner, final String name, final String desc) {
+                    // todo: detect try-with-resources
+//                    if (name.equals("addSuppressed")) {
+//                        ...
+//                    }
+                    super.visitMethodInsn(opcode, owner, name, desc);
+                }
+            };
+        }
+    }
+}
